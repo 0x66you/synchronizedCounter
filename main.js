382c6f4013ffed0e72fb3cc12f4486bd23d27c33
YehChunChen_ <91467203+0x66you@users.noreply.github.com>
2021-10-25 22:26:53 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+const state = {
+    counter:0
+}
+
+const getters = {
+    counter(state){
+        return state.counter
+    }
+}
+
+const mutations = {
+    Increment: (state,payload) => state.counter+= payload,
+    Decrement: (state,payload) => state.counter-=payload
+}
+
+const actions = {
+    increment: (context,payload) => context.commit('Increment',payload),
+    decrement: (context,payload) => context.commit('Decrement',payload)
+}
+Vue.component('counter1',{
+    template:'#counter1UI',
+    computed:{
+        ...Vuex.mapState(['counter']),
+
+    },
+    methods:{
+        ...Vuex.mapActions(['increment','decrement'])
+    }
+})
+Vue.component('counter2',{
+    template:'#counter2UI',
+    computed:{
+        ...Vuex.mapState(['counter']),
+    },
+    methods:{
+        ...Vuex.mapActions(['increment','decrement'])
+    }
+})
+const store = new Vuex.Store({
+    state,
+    getters,
+    mutations,
+    actions
+})
+new Vue({
+    el:'#app',
+    store
+})
---
+++ main2.js
@@ -0,0 +1,50 @@
+const eventBus = new Vue({
+    data:{
+        counter:0
+    }
+})
+
+Vue.component('counter1',{
+    template:'#counter1UI',
+    computed:{
+        counter(){
+            return eventBus.counter
+        }
+    },
+    methods:{
+        increment(step){
+            eventBus.$emit('increment',step)
+        },
+        decrement(step){
+            eventBus.$emit('decrement',step)
+        }
+    }
+})
+Vue.component('counter2',{
+    template:'#counter2UI',
+    computed: {
+        counter(){
+            return eventBus.counter
+        }
+    },
+    methods:{
+        increment(step){
+            eventBus.$emit('increment',step)
+        },
+        decrement(step){
+            eventBus.$emit('decrement',step)
+        }
+    }
+})
+
+new Vue({
+    el:'#app',
+    created(){
+        eventBus.$on('increment',value=>{
+            eventBus.counter += value
+        })
+        eventBus.$on('decrement',value=>{
+            eventBus.counter -= value
+        })
+    }
+})
